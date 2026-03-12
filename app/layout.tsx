dd5c4d521eeb1c401900dbecb7b57ada73301ce4
HP-Probook <emmanuel19727@gmail.com>
2026-03-12 12:47:19 +0300

    upd icn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/lib/auth-context";
+
+export const metadata: Metadata = {
+  title: "RunnerMKT | Admin",
+
+  icons: {
+    icon: "/images/icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
